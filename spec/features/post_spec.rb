830d3261e528e3e45572c6e2b2136c66fbd12abf
Jasmine Carter <jasmine.carter87@gmail.com>
2020-10-03 00:09:32 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -12,4 +12,18 @@ describe 'new post' do
     expect(page).to have_content('Post Form')
   end
 
+
+  it "isplays a new post form that reidrects to the index page, which then contains the submitted post's title and description"
+  do
+    visit new_post_path
+    fill_in 'post_description', with: "My post title"
+    fill_in 'post_description', with: "My post description"
+
+    click_on "Submit Post"
+
+    expect(page.current_path).to eq(posts_path)
+    expect(page).to have_content('My post title')
+    expect(page).to have_content('My post description')
+  end
+
 end
